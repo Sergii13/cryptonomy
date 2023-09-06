8d39f4808d6d571c8aada148273a9a2a1a6e0b7b
sergii13 <sergii.khilchuk13@gmail.com>
2023-09-06 15:59:55 +0300

    add new block

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -10,3 +10,30 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+let videoBlock = document.querySelectorAll(".video");
+for (let i = 0; i < videoBlock.length; i++) {
+  let btn = videoBlock[i].querySelector(".video__btn");
+  let videoThumb = videoBlock[i].querySelector(".video__image-prev");
+  let videoIframe = videoBlock[i].querySelector(".video__iframe");
+  btn.addEventListener("click", function () {
+    let iframe = videoBlock[i].querySelector(".video-iframe");
+    let videoSrc = videoBlock[i].getAttribute("data-video");
+    let player = new YT.Player(videoIframe, {
+      videoId: "" + videoSrc + "",
+      playerVars: {
+        rel: "0",
+      },
+      events: {
+        onReady: onPlayerReady,
+      },
+    });
+    videoBlock[i].classList.add("active");
+  });
+  const onPlayerReady = function (event) {
+    event.target.playVideo();
+    if (videoBlock[i].getElementsByTagName("iframe")[0]) {
+      videoThumb.remove();
+    }
+  };
+}
